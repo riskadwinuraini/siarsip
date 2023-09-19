fc8d1f9b0baf9ed15e084a6ec436006c056064c9
destroylord <masapin68@gmail.com>
2023-09-19 15:44:29 +0700

    seeder: change DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
+            RoleSeeder::class, 
+            UserSeeder::class,
             TypeFileSeeer::class
-            // RoleSeeder::class, UserSeeder::class
         ]);
     }
 }
